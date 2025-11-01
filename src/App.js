5ba2d613ddfae487676bfb63448f911f51d66146
Gabii <gabrielakarinalopez01@gmail.com>
2025-11-01 20:10:42 -0300

    cambiar a hashRouter para GitPages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import {  HashRouter, Routes, Route } from 'react-router-dom';
 import Inicio from './views/Inicio';
 import Login from './views/Login';
 import DatosPersonales from './views/DatosPersonales';
@@ -8,20 +8,20 @@ import Calendario from './views/Calendario';
 import Asistencia from './views/Asistencia';
 import Informacion from './views/Informacion';
 
-const router = createBrowserRouter([
-  { path: '/', element: <Inicio /> },
-  { path: '/login', element: <Login /> },
-  { path: '/datos-personales', element: <DatosPersonales /> },
-  { path: '/compromisos', element: <Compromisos /> },
-  { path: '/calendario', element: <Calendario /> },
-  { path: '/asistencia', element: <Asistencia /> },
-  {path: '/informacion', element: <Informacion /> }
-], {
-  basename: 'agenda-soy-mas-deploy'
-});
-
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Inicio />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/datos-personales" element={<DatosPersonales />} />
+        <Route path="/compromisos" element={<Compromisos />} />
+        <Route path="/calendario" element={<Calendario />} />
+        <Route path="/asistencia" element={<Asistencia />} />
+        <Route path="/informacion" element={<Informacion />} />
+      </Routes>
+    </HashRouter>
+  );
 }
 
 export default App;
